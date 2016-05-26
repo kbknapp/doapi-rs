316ceb02cedad764c18aacc0ad5e0e3e750807cf
Aidan Hobson Sayers <aidanhs@cantab.net>
2016-05-26 18:36:41 +0100

    Remove duplicate code

--- src/request/dorequest.rs
+++ src/request/dorequest.rs
@@ -38,43 +38,14 @@ pub trait DoRequest<T> : BaseRequest
     }
 
     fn retrieve_json(&self) -> hyper::Result<String> {
-        let url = match Url::parse(self.url()) {
-            Ok(url) => url,
-            Err(e) => return Err(Error::Uri(e)),
-        };
-        let mut fresh_req = match client::Request::new(self.method(), url) {
-            Ok(req) => req,
-            Err(e) => return Err(e),
-        };
-        let mut auth_s = String::new();
-        auth_s.push_str("Bearer ");
-        auth_s.push_str(self.auth());
-        fresh_req.headers_mut().set(ContentType("application/json".parse().unwrap()));
-        fresh_req.headers_mut().set(Authorization(auth_s));
-        let mut streaming_req = try!(fresh_req.start());
-        if let Some(ref b) = self.body() {
-            streaming_req.write(b.as_bytes()).unwrap();
-        }
-        let mut response = try!(streaming_req.send());
+        let mut response = try!(self.retrieve_raw_response());
         let mut s = String::new();
         try!(response.read_to_string(&mut s));
         Ok(s)
     }
 
     fn retrieve_raw_response(&self) -> hyper::Result<client::response::Response> {
-        let url = match Url::parse(self.url()) {
-            Ok(url) => url,
-            Err(e) => return Err(Error::Uri(e)),
-        };
-        let mut fresh_req = match client::Request::new(self.method(), url) {
-            Ok(req) => req,
-            Err(e) => return Err(e),
-        };
-        let mut auth_s = String::new();
-        auth_s.push_str("Bearer ");
-        auth_s.push_str(self.auth());
-        fresh_req.headers_mut().set(ContentType("application/json".parse().unwrap()));
-        fresh_req.headers_mut().set(Authorization(auth_s));
+        let fresh_req = try!(self.request());
         let mut streaming_req = try!(fresh_req.start());
         if let Some(ref b) = self.body() {
             streaming_req.write(b.as_bytes()).unwrap();
